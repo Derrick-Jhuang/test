2d0a0579944b707f81ccdb19c4c90394d9e39d99
Derrick Jhuang <jhuangping9981@gmail.com>
2025-10-22 18:35:40 +0800

    update yd004

--- web/yd004_renjyi/scripts/custom.js
+++ web/yd004_renjyi/scripts/custom.js
@@ -1,14 +1,24 @@
 // device
-const device = new getDevice(1280, false);
+// 判斷是否為觸控+行動裝置
+const isMobileTouch = () => device.isTouchDevice() && device.isMobile().any;
+const getWindowSize = () => device.windowSize;
 
-// 偵測是否為行動裝置
-// console.log(device.isMobile().any); // true/false
+// 根據結果動態切換 body class
+const updateDeviceClass = () => {
+  document.body.classList.toggle('mobile', isMobileTouch());
+  document.body.classList.toggle('desktop', !isMobileTouch());
 
-// 偵測是否為觸控裝置
-// console.log(device.isTouchDevice()); // true/false
+  const getWinW =  getWindowSize().width;
+  if (getWinW > 1200) {
+    $('.c-sidebar__ctr').removeClass('is-open');
+  }
+};
 
-// 取得螢幕尺寸
-// console.log(device.getWindowSize()); 
+// 建立偵測器，並在狀態變更時執行 updateDeviceClass
+const device = new getDevice(1280, updateDeviceClass, false);
+
+// 立即執行一次以初始化 class
+updateDeviceClass();
 
 // store
 const useStore = new LocalStore('useData',{isPlicyShow : false});
@@ -18,7 +28,9 @@ const $cookie = document.querySelector('.c-cookie');
 
 // policy
 (function policy() {
-  !$store.isPlicyShow && $cookie.classList.add('an-in');
+  if ($cookie) {
+    !$store.isPlicyShow && $cookie.classList.add('an-in');
+  }
 })();
 
 const closeCookie = () => {
@@ -47,6 +59,39 @@ jhuangPingJqTool.click({
 
 // imgfit
 imgfit.init();
+
+// menu
+(function menuToggle() {
+  $('.hd .is-mobile .c-btn--toggle').click(function (e) {
+    e.stopPropagation();
+    $('.hd .is-mobile .menu').toggleClass('is-menu-open');
+    $('.hd').toggleClass('is-menu-open');
+  })
+  
+  $('.hd .is-mobile .menu-close').click(function (e) {
+    e.stopPropagation();
+    $('.hd .is-mobile .menu').removeClass('is-menu-open');
+    $('.hd').removeClass('is-menu-open');
+  })
+  
+  $(document).on('click', function () {
+    $('.hd .is-mobile .menu').removeClass('is-menu-open');
+    $('.hd').removeClass('is-menu-open');
+  });
+})();
+
+// products
+(function proInit() {
+  $('.c-sidebar-toggle').click(function (e) {
+    e.stopPropagation();
+    $('.c-sidebar__ctr').toggleClass('is-open');
+  })
+
+  $('.c-sidebar__mask').click(function (e) {
+    e.stopPropagation();
+    $('.c-sidebar__ctr').removeClass('is-open');
+  })
+})();
 
 // lightbox
 const openPrivacyLbx = () => FetchLightbox.open({
